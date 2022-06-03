0406a46c6c8223e54b177826ba7ec8db8119eadf
Raisa Bernardes <raisa.rmro@gmail.com>
2022-06-03 16:25:12 -0300

    condicoes

---
+++ javascript-basico/5-condicoes.js
@@ -0,0 +1,11 @@
+//console.log('O console funcionou corretamente!') //no node.js, para escrever na tela, não funciona o document.write
+
+var vel = 78
+
+console.log(`Sua velocidade atual é ${vel}Km/h`)
+
+if(vel > 60){ //condição simples (sem 'else')
+    console.log('Você ultrapassou o limite de velocidade! MUTADO!')
+}
+console.log('Dirija sempre com o cinto de segurança!')
+
