9104085299d0d83f0f7c98a31a791ecf062dec44
Raisa Bernardes <raisa.rmro@gmail.com>
2022-06-14 15:40:48 -0300

    Primeira revisão de métodos para lidar com o DOM

--- javascript-basico/exercicios/exercicio6-calculadora/script.js
+++ javascript-basico/exercicios/exercicio6-calculadora/script.js
@@ -1,10 +1,16 @@
+'use strict';
+
 //1 - declarar variaveis
+const display = document.getElementById('display')
+const numeros = document.querySelectorAll('[id*=tecla]') //pega todos os elementos que tem a palavra "tecla" no id e coloca num array
 
-//2 - capturar o click das telas
+const inserirNumero = (evento) => display.textContent = evento.target.textContent
+
+//2 - capturar o click de cada uma das telas
+numeros.forEach(numero => numero.addEventListener('click',inserirNumero)) //pega um número e adiciona um click. Quando clicar ele vai chamar o método "inserirNumero()" 
 
 
-
-
+//O for each varre todos os elementos de um array.
 
 
 
