64353f45190a87003cbfbf47e94fec9911d17196
Raisa Bernardes <raisa.rmro@gmail.com>
2022-06-15 17:44:14 -0300

    setTimeOut e setInterval

--- javascript-basico/conteudo/12-Introdução-objetos.js
+++ javascript-basico/conteudo/12-Introdução-objetos.js
@@ -8,3 +8,9 @@ engordar(p=0){
 }}
 
 console.log(amigo)
+
+
+
+// - O this segue o mesmo raciocínio do java
+// - Fora de escopos locais o this sempre se refere ao objeto Window
+// - Em objetos o this vai se referir a instância e pode acessar suas propriedades
