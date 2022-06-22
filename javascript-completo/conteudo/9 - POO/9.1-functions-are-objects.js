c106922528d323b7c743b4645f18e28fc67fbc33
Raisa Bernardes <raisa.rmro@gmail.com>
2022-06-22 18:33:31 -0300

    + functions

---
+++ javascript-completo/conteudo/9 - POO/9.1-functions-are-objects.js
@@ -0,0 +1,19 @@
+//Every object in JS has a property called constructor, and that references the function 
+//that was used to create the object
+
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function(){
+        console.log('draw')
+    }
+}
+
+const Circle1 = new Function('radius', `
+    this.radius = radius;
+    this.draw = function(){
+    console.log('draw')
+    }
+`);
+
+const circle = new Circle1(1); //Te same as: Circle.call({}, 1)
+
