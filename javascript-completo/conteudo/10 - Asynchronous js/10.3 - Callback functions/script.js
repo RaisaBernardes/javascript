fb454f2c7b84513bdd733db1fd61ea615178c3d7
Raisa Bernardes <raisa.rmro@gmail.com>
2022-07-04 15:47:56 -0300

    callbacks overview

--- javascript-completo/conteudo/10 - Asynchronous js/10.3 - Callback functions/script.js
+++ javascript-completo/conteudo/10 - Asynchronous js/10.3 - Callback functions/script.js
@@ -1,16 +1,26 @@
-const request = new XMLHttpRequest(); 
+const getTodos = (callback) => {
+   
+    const request = new XMLHttpRequest(); 
 
+    request.addEventListener('readystatechange',  () => {
+        //console.log(request, request.readyState); 
+        if(request.readyState === 4 && request.status === 200){ 
+            callback(undefined, request.responseText); //undefined for the error
+        } else if (request.readyState === 4){
+            callback('could not fetch data', undefined); //undefined for the data
+        }
+    });
 
-request.addEventListener('readystatechange',  () => {
-    //console.log(request, request.readyState); 
-    if(request.readyState === 4 && request.status === 200){ 
-        console.log(request.responseText);
-    } else if (request.readyState === 4){
-        
-        console.log('could not fetch the data')
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/'); 
+    request.send();
+}
+
+getTodos((err, data) => { //when we do callbacks like this, is a convention do to the error first and then the data
+    console.log('callback fired')
+    if(err){
+        console.log(err)
+    }else{
+        console.log(data)
     }
 });
 
-
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/'); 
-request.send();
