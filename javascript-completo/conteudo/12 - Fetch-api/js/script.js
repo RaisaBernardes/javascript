11b0869366e763c1740bd70b04d59ae86243c421
Raisa Bernardes <raisa.rmro@gmail.com>
2023-01-24 15:52:44 -0300

    API Fetch - Done: createComment

--- javascript-completo/conteudo/12 - Fetch-api/js/script.js
+++ javascript-completo/conteudo/12 - Fetch-api/js/script.js
@@ -58,6 +58,34 @@ async function getPost(id){
     loadingElement.classList.add("hide");
     postPage.classList.remove("hide");
 
+    const title = document.createElement("h1");
+    const body = document.createElement("P");
+
+    title.innerText = dataPost.title;
+    body.innerText = dataPost.body;
+    
+    postContainer.appendChild(title);
+    postContainer.appendChild(body);
+
+
+    dataComments.map((comment) => { //comment é a unidade de dataComments
+        createComment(comment);
+    })
+
+}
+
+function createComment(comment){ //vou receber um comentário
+    const div = document.createElement("div");
+    const email = document.createElement("h3");
+    const commentBody = document.createElement("p");
+
+    email.innerText = comment.email;
+    commentBody.innerText = comment.body;
+
+    div.appendChild(email);
+    div.appendChild(commentBody);
+
+    commentsContainer.appendChild(div);
 }
 
 if(!postId) {
